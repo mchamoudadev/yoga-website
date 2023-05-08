49017c340ad46c9400ab6cff020e0007338e10c5
mchamoudadev <mchamuuda@gmail.com>
2023-05-08 21:04:31 +0300

    changed all of them

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ const icons = document.querySelectorAll("i");
 hamburger.addEventListener("click", function (event) {
     const isVisible = mobileMenu.getAttribute('data-visible');
     if (isVisible == "true") {
-        // qari
+        // qari hiding
         mobileMenu.setAttribute('data-visible', "false");
         icons[0].setAttribute('data-visible', "true");
         icons[1].setAttribute('data-visible', "false");
     } else if (isVisible == "false") {
-        // soo bandhig
+        // soo bandhig showing
         mobileMenu.setAttribute('data-visible', "true");
         icons[0].setAttribute('data-visible', "false");
         icons[1].setAttribute('data-visible', "true");
